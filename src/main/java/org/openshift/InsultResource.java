82784f0ec9f07ef2f30f0120b842e9258968e2de
rishish <rishish@live.com>
2019-07-02 10:28:42 -0400

    another compilation error

--- src/main/java/org/openshift/InsultResource.java
+++ src/main/java/org/openshift/InsultResource.java
@@ -11,7 +11,7 @@ import javax.ws.rs.Produces;
 public class InsultResource {
     @GET()
     @Produces("application/json")
-    public HashMap<String, String> getInsult() {
+    public HashMap<String, String> getInsult() throws Exception {
         HashMap<String, String> theInsult = new HashMap<String, String>();
         theInsult.put("insult", new InsultGenerator().generateInsult());
         return theInsult;
